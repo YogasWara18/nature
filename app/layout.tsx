5bc18221ae3e492d58162298ceab0d74002884fe
YogasWara18 <teukukurniayogaswara@gmail.com>
2025-12-02 23:50:20 +0700

    First Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,22 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Audiowide, Cal_Sans, Golos_Text } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const audiowide = Audiowide ({
+  weight: "400",
+  variable: "--font-audiowide",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const golostext = Golos_Text ({
+  weight: "400",
+  variable: "--font-golostext",
+  subsets: ["latin"],
+});
+
+const calsans = Cal_Sans ({
+  weight: "400",
+  variable: "--font-calsans",
   subsets: ["latin"],
 });
 
@@ -25,7 +33,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${audiowide.variable} ${golostext.variable} ${calsans.variable}`}
       >
         {children}
       </body>

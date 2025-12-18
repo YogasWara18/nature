c64360a1f8940f3f7a39b65f09fa527c498c859a
YogasWara18 <teukukurniayogaswara@gmail.com>
2025-12-18 22:25:53 +0700

    Banner update

--- app/UI-Components/Index/Banner/page.tsx
+++ app/UI-Components/Index/Banner/page.tsx
@@ -1,24 +1,17 @@
+import Link from "next/link";
+
 export default function Banner() {
   return (
     <>
       <div className="px-[8%] lg:px-[12%] py-30 banner h-[50vh] md:h-[90vh] flex flex-col items-center justify-center">
-        <div className="title flex flex-col lg:flex-row gap-10">
-          <span className="rounded-full title-span border border-gray-400 px-6 GolosText uppercase font-bold">
-            Our Services
-          </span>
-          <div className="w-full lg:w-2/3">
-            <h1 className="CalSans text-4xl md:text-5xl">
-              Crafting Interiors Connecting with
-              <span className="text-(--prim)"> Nature</span>
-            </h1>
-            <p className="text-gray-400 GolosText">
-              We specialize in creating interiors that harmonize modern design
-              with the timeless beauty of nature. Every space we craft is more
-              than just visually stunning it’s a sanctuary that nurtures well
-              being, inspires creativity, and strengthens the human connection
-              to the natural world.
-            </p>
-          </div>
+        <div className="title flex flex-col items-center justify-center">
+          <span className="rounded-full text-white hero-span border border-gray-400 px-6 mt-2 GolosText uppercase font-bold">Inspired by Nature</span>
+          <h1 className="CalSans text-4xl md:text-5xl w-full lg:w-100% my-5 text-white">Let’s Grow Your <span className="text-(--prim)"> Dream Project</span> Naturally</h1>
+          <Link href="/UI-Components/Pages/Contact">
+            <button className="border border-gray-300 GolosText text-white rounded-full mt-3 px-4 py-2 hover:bg-(--prim) hover:text-white cursor hover:border-transparent transition-all duration-300">
+                    Start With Nature
+                  </button>
+          </Link>
         </div>
       </div>
     </>

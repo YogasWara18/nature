02115b05ac46268eb8566296dcd2c8742ff011ab
YogasWara18 <teukukurniayogaswara@gmail.com>
2025-12-20 05:51:19 +0700

    Update Newsletter

--- app/UI-Components/Index/Newsletter/page.tsx
+++ app/UI-Components/Index/Newsletter/page.tsx
@@ -10,7 +10,7 @@ export default function Newsletter() {
                  <p className="text-center GolosText text-white w-full text-lg lg:w-[60%] mb-5">Join our Nature Journal. Discover organic ideas, access serene inspirations, and stay connected with harmonious design updates.</p>  
 
                  <div className="email-details border-b border-gray-300 w-[450px] flex items-center transition-all duration-300 hover:border-(--prim) cursor-pointer">
-                    <input type="email" placeholder="Email address..." className="focus:outline-none w-full py-3"/>
+                    <input type="email" placeholder="Email address..." className="text-white focus:outline-none w-full py-3"/>
                     <i className="bi bi-arrow-up-right px-2 py-1 bg-(--prim) text-white rounded-full transition-all duration-300 hover:rotate-45"></i>
                  </div>
             </div>    
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Audiowide, Cal_Sans, Golos_Text } from "next/font/google";
 import "./globals.css";
 import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
 
 const audiowide = Audiowide ({
   weight: "400",
@@ -38,6 +39,7 @@ export default function RootLayout({
       >
         <Navbar/>
         {children}
+        <Footer/>
       </body>
     </html>
   );

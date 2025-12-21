d0660627389890987d47ec00f17029f1791933a0
YogasWara18 <teukukurniayogaswara@gmail.com>
2025-12-21 23:08:26 +0700

    Update projects page

--- app/UI-Components/Projects/page.tsx
+++ app/UI-Components/Projects/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import Link from "next/link";
+import ProjectsData from "@/app/JsonData/Projects.json"
+import { div } from "framer-motion/client";
 
 
 export default function Projects() {
@@ -17,6 +19,38 @@ export default function Projects() {
           </Link>
           <i className="ri-arrow-right-wide-fill mt-1"></i>
           <h2 className="GolosText">Projects</h2>
+        </div>
+      </div>
+
+      <div className="px-[8%] lg:px-[12%] py-30">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+            {ProjectsData.map((project, index) => (
+                <div key={index}>
+                    <div className="project-card group">
+                        <Link href={`/UI-Components/Projects/${project.id}`}>
+                            <div className="project-image relative w-full h-[550px] overflow-hidden rounded-2xl cursor-pointer">
+                                <div className="project-img w-full h-full bg-cover group-hover:scale-110 transition-all duration-500 bg-center"
+                                style={{ backgroundImage: `url(${project.image})` }}>
+                                </div>
+                                <div className="absolute top-5 left-5 flex gap-2 z-50">
+                                    {project.tags.map((tag, idx) => (
+                                        <div key={idx} className="bg-white/20 backdrop-blur-sm text-white border border-gray-300 px-4 py-1 rounded-full hover:bg-(--prim) hover:border-transparent transition-all duration-300">
+                                            {tag}
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        </Link>
+                        <div className="project-info my-5">
+                            <Link href={`/UI-Component/Projects/${project.id}`}>
+                                    <h2 className="GolosText font-semibold text-4xl hover:text-(--prim) transition-all duration-300">{project.title}</h2>
+                            </Link>
+                            <h5 className="GolosText text-2xl mt-2">{project.location}</h5>
+                            <p className="GolosText text-md text-ggray-700">{project.date}</p>
+                        </div>
+                    </div>
+                </div>
+            ))}
         </div>
       </div>
     </>

0c0d920c22650f389c0abbf63bf8c5fa4dbdfc3e
YogasWara18 <teukukurniayogaswara@gmail.com>
2025-12-22 00:05:20 +0700

    Update Projects page done

---
+++ app/UI-Components/Projects/[id]/page.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function ProjectDetails() {
+  return (
+    <>
+         <div className="section-bg text-white flex flex-col">
+        <h1 className="text-8xl GolosText pt-10">Projects</h1>
+        <div className="flex items-center text-xl mt-3">
+          <Link
+            href="/"
+            className="hover:text-(--prim) transition-all duration-300"
+          >
+            Home
+          </Link>
+          <i className="ri-arrow-right-wide-fill mt-1"></i>
+          <h2 className="GolosText">Projects</h2>
+        </div>
+      </div>
+    </>
+  )
+}

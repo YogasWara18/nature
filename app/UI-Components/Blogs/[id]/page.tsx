c5aac2f9f4b248f63bf5a55201310146712232b3
YogasWara18 <teukukurniayogaswara@gmail.com>
2025-12-22 22:55:04 +0700

    Update Blog

--- app/UI-Components/Blogs/[id]/page.tsx
+++ app/UI-Components/Blogs/[id]/page.tsx
@@ -8,7 +8,7 @@ import Image from "next/image";
 
 import blogDet1 from "@/public/Blog-details-1.jpg";
 import blogDet2 from "@/public/Blog-details-2.jpg";
-import quote from "@/public/Blog-quote.png";
+import quote from "@/public/navbar-logo.png";
 
 
 export default function BlogDetails() {
@@ -98,6 +98,33 @@ export default function BlogDetails() {
                         alt="blogDet2"
                         className="w-full h-full rounded-2xl"
                     />
+                </div>
+                <h2 className="text-4xl md:text-4xl CalSans my-5">Exploring Design Styles</h2>
+                <p className="text-gray-500 GolosText mt-5 leading-relaxed">
+                    Nature inspired interior design emphasizes the harmony between organic beauty and practical function, featuring multi purpose furniture crafted from sustainable materials such as extendable wooden dining tables, bamboo sofa beds, or modular rattan shelving units that provide storage while maintaining a warm, natural aesthetic; ergonomic seating and eco friendly workspaces enhance comfort and well being, especially in home offices, while earthy tones, raw textures, and biophilic elements ensure every piece contributes to a balanced, serene living space deeply connected to nature.
+                </p>
+                <div className="my-5">
+                    <div className="bg-[#efebe8] rounded-2xl flex flex-col justify-center items-center text-center px-10 py-8">
+                        <Image
+                            src={quote}
+                            alt="Quote"
+                            className="rounded-2xl w-50"
+                        />
+                        <p className="GolosText text-2xl mb-3">“Nature whispers balance, teaches resilience, and inspires peace in its quiet embrace, we find beauty, strength, and the timeless harmony of living connected to earth.”</p>
+                        <h4 className="GolosText font-bold">NATURE</h4>
+                    </div>
+                </div>
+                <h2 className="tex-4xl md:text-4xl CalSans my-5">Bringing Modern Interior Design</h2>
+                <p className="text-gray-500 GolosText mt-5 leading-relaxed">
+                    Incorporating these nature inspired interior design principles, you can transform your home into a serene, organic, and welcoming haven. Whether you’re making small updates or embarking on a full renovation, focusing on natural materials, earthy tones, biophilic elements, and sustainable functionality enhances both beauty and comfort. By embracing sunlight, greenery, and eco friendly furniture, your living space becomes not only stylish and practical but also deeply connected to the calming essence of nature.
+                </p>
+            </div>
+            <div className="w-full lg:w-1/2 sticky top-22 left-0 h-full">
+                <div>
+                    <h2 className="text-4xl md:text-4xl CalSans my-5">Categories</h2>
+                    <div className="flex flex-col gap-2">
+                        <h2 className="GolosText text-lg hover:text-(--prim) cursor-pointer transition-all duration-300 font-semibold border-b border-gray-300 pb-1">Accessories</h2>
+                    </div>
                 </div>
             </div>
         </div>
